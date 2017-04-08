9645e75b26130ec8f098c4751987567568a7a2a5
Matthew J. Martin <matthew.mar10@gmail.com>
2017-04-08 08:09:31 -0700

    adjust test case to real life problem

--- test/test.js
+++ test/test.js
@@ -1663,60 +1663,56 @@ test('original various tests', function (t) {
 });
 
 test('map array inside array to property', function (t) {
+
+  var mongoose = require('mongoose');
+
   var obj = {
-    name: 'Transfer in Sunnyvale',
-    transfers: [{
-      type: 'TAKE',
-      target_route: {
-        _id: '58e4a15607689eafed8e2840',
-        driver: '58e4a15607689eafed8e282a'
-      },
-      orders: [ '58e4a15807689eafed8e2d0b' ]
-    }, {
-      type: 'GIVE',
-      target_route: {
-        _id: '58e4a15607689eafed8e2840',
-        driver: '58e4a15607689eafed8e282a'
-      },
-      orders: [ '58e4a15607689eafed8e2841' ]
-    }],
-    orders: [
-      '58e4a15807689eafed8e2d0b',
-      '58e4a15607689eafed8e2841'
-    ]
-  };
+  "orders": [{
+    "foodie": {
+      "first_name": "Foodie2",
+      "last_name": "Foodie2"
+    },
+    "sort_code": "A02"
+  }],
+  "transfers": [{
+    "type": "GIVE",
+    "target_route": {
+      "_id": "58e4a15607689eafed8e2841",
+      "driver": "58e4a15607689eafed8e2831"
+    },
+    "orders": ["58e4a15807689eafed8e2d0b"]
+  }]
+};
 
   var expect = {
-    name: 'Transfer in Sunnyvale',
-    transfers: [{
-      type: 'TAKE',
-      target_route: {
-        _id: '58e4a15607689eafed8e2840',
-        driver: '58e4a15607689eafed8e282a'
-      },
-      orders: [ '58e4a15807689eafed8e2d0b' ]
-    }, {
-      type: 'GIVE',
-      target_route: {
-        _id: '58e4a15607689eafed8e2840',
-        driver: '58e4a15607689eafed8e282a'
-      },
-      orders: [ '58e4a15607689eafed8e2841' ]
-    }],
-    orders: [
-      '58e4a15807689eafed8e2d0b',
-      '58e4a15607689eafed8e2841'
-    ]
-  };
+  "orders": [{
+    "foodie": {
+      "first_name": "Foodie2",
+      "last_name": "Foodie2"
+    },
+    "sort_code": "A02"
+  }],
+  "transfers": [{
+    "type": "GIVE",
+    "target_route": {
+      "_id": "58e4a15607689eafed8e2841",
+      "driver": "58e4a15607689eafed8e2831"
+    },
+    "orders": ["58e4a15807689eafed8e2d0b"]
+  }]
+};
 
   // would expect this to just assign the array as a property
   var map = {
-    'name': 'name',
+    'orders[]._id': 'orders[]._id',
+    'orders[].sort_code': 'orders[].sort_code',
+    'orders[].foodie._id': 'orders[].foodie._id',
+    'orders[].foodie.first_name': 'orders[].foodie.first_name',
+    'orders[].foodie.last_name': 'orders[].foodie.last_name',
     'transfers[].type': 'transfers[].type',
-    'transfers[].target_route._id': 'transfers[].target_route._id',
-    'transfers[].target_route.driver': 'transfers[].target_route.driver',
     'transfers[].orders[]': 'transfers[].orders',
-    'orders': 'orders'
+    'transfers[].target_route._id': 'transfers[].target_route._id',
+    'transfers[].target_route.driver': 'transfers[].target_route.driver'
    };
 
   var result = om(obj, map);
