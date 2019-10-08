d3a2a570100a889cb37df38fd3f082ae5818cfc9
Scott Switzer <switzer@switzer.org>
2019-10-08 12:33:19 -0400

    added performance profiling, starting tuning

--- src/object-mapper.js
+++ src/object-mapper.js
@@ -1,173 +1,237 @@
 'use strict';
 
 var _undefined
+, performance = require('perf_hooks').performance
 
 function ObjectMapper(from_object, to_object, property_map)
 {
+  global.elapsed['ObjectMapper'].n++ // performance monitoring
+
   // There are a few different constructors - move around properties if needed
   if (typeof property_map === 'undefined') {
     property_map = to_object;
     to_object = _undefined
   }
-  return map(from_object, to_object, property_map)
-}
 
-function map(from_object, to_object, property_map)
-{
-  // The property map is an object where the key is a string representing a notation where to look for data in an object
-  //  The value is either a string, array, or object denoting how to place data into the new object
-  //  The goal is to take in input and to have a standard way of passing in a list of values:
-  //   from_key = array of strings representing either an object or array
-  //   to_key   = array of objects where:
-  //     name     = key,
-  //     function = the function used to process the information for the key
-  //     default  = value if there is no information about the key
   for (const [from_key, to_key] of Object.entries(property_map)) {
+
+    // Traverse the 'from' object
     var from_data = getKeyValue(from_object, from_key)
+ 
+    // Insert into the 'to' object
     to_object = setKeyValue(to_object, to_key, from_data)
   }
+
   return to_object
 }
 
-// d1[0] = 'a'
-// d2[1] = 'b'
-// d3[2] = 'c'
-
-// joe.mary[].sally = d1:3
-// joe.mary[].jane = e1:3
 // if the data is an array, walk down the obj path and build until there is an array key
-
-function update(obj, key_arr, data, _default, transpose)
+function update(obj, key_arr, data)
 {
+  global.elapsed['update'].n++ // performance monitoring
+  let timer = performance.now()
+  let x = 0 // index of the key array
+
+  var o
   // Get the object key and index that needs to be parsed
   var [key, ix] = process(key_arr.shift())
 
   // If there is a key, we need to traverse down to this part of the object
   if (key) {
-    // If the object is undefined, we need to create a new object
-    if (obj == null) obj = {}
-    // Set the key of the object equal to the recursive, or if at the end, the data
-    if (key_arr.length > 0) {
-      obj[key] = update(obj[key], key_arr, data, _default, transpose)
-    } else {
-      // This is a leaf.  Set either the data or default value into the obj
-      if (data) obj[key] = data
-      else if (_default) obj[key] = _default
-    }
-    return obj
+    global.elapsed['update'].ms += (performance.now() - timer)
+    o = update_obj(obj, key, key_arr, data)
+    timer = performance.now()
   }
 
   if (ix !== null) {
-    // If the top level object is undefined, we need to create a new array
-    if (obj == null) obj = []
-    // Make sure that there is an array item for each item in the data array
-    if (Array.isArray(data)) {
-      obj = data.reduce(function(o,d,i) {
-        if (i == '' || i == i)
-          o[i] = update(o[i], key_arr.slice(), d, _default, transpose)
-        return o
-      }, obj)
-      return obj
-    }
-    // If there is more work to be done, push an object onto the array
-    else {
-      var x = (ix) ? ix : 0
-      obj[x] = (key_arr.length > 0) ? update(obj[x], key_arr, data, _default, transpose) : (data || _default)
-    }
+    global.elapsed['update'].ms += (performance.now() - timer)
+    o = update_arr(obj, key, ix, key_arr, data)
+    timer = performance.now()
   }
 
+  global.elapsed['update'].ms += (performance.now() - timer)
+  return o
+}
+
+function update_obj(obj, key, key_arr, data)
+{
+  global.elapsed['update_obj'].n++ // performance monitoring
+  let timer = performance.now()
+
+  // If the object is undefined, we need to create a new object
+  if (obj == null) obj = {}
+  // Set the key of the object equal to the recursive, or if at the end, the data
+  if (key_arr.length > 0) {
+    global.elapsed['update_obj'].ms += (performance.now() - timer)
+    obj[key] = update(obj[key], key_arr, data)
+    timer = performance.now()
+  } else {
+    // This is a leaf.
+    if (data) obj[key] = data
+  }
+  global.elapsed['update_obj'].ms += (performance.now() - timer)
   return obj
 }
 
+function update_arr(obj, key, ix, key_arr, data)
+{
+  global.elapsed['update_arr'].n++ // performance monitoring
+  let timer = performance.now()
+
+  // If the top level object is undefined, we need to create a new array
+  if (obj == null) obj = []
+  // Make sure that there is an array item for each item in the data array
+  if (Array.isArray(data)) {
+    obj = data.reduce(function(o,d,i) {
+      if (i == '' || i == i) {
+        global.elapsed['update_arr'].ms += (performance.now() - timer)
+        o[i] = update(o[i], key_arr.slice(), d)
+        timer = performance.now()
+        return o
+      }
+    }, obj)
+    return obj
+  }
+  // If there is more work to be done, push an object onto the array
+  else {
+    var x = (ix) ? ix : 0
+    if (key_arr.length > 0) {
+      global.elapsed['update_arr'].ms += (performance.now() - timer)
+      obj[x] = update(obj[x], key_arr, data)
+      timer = performance.now()
+    } else {
+      global.elapsed['update_arr'].ms += (performance.now() - timer)
+      obj[x] = data
+      timer = performance.now()
+    }
+  }
+
+  global.elapsed['update_arr'].ms += (performance.now() - timer)
+  return obj
+}
+  
 function select(obj, key_arr)
 {
+  global.elapsed['select'].n++ // performance monitoring
+  let timer = performance.now()
+
   // Check to see if key_arr is not an array.  If so, wrap it in an array.
   // This is done for backwards compatibility from external functions that pass in a string
   if (!Array.isArray(key_arr))
     key_arr = parse(key_arr)
 
-  // If we are at the end of the key array stack, return the object
-  if (key_arr.length == 0) return obj
+  // If we are at the end of the key array x`stack, return the object
+  if (key_arr.length == 0) {
+    global.elapsed['update_arr'].ms += (performance.now() - timer)
+    return obj
+  }
 
   // Get the object key or index that needs to be parsed
+  global.elapsed['update_arr'].ms += (performance.now() - timer)
   var [key, ix] = process(key_arr.shift())
+  timer = performance.now()
 
   // If there is an object key, grab the object property
   if (key) {
-    // If the underlying object is an array, select the first element
-    if (Array.isArray(obj) && obj.length > 0) obj = obj.shift()
-    // If there is no object property associated with this key, return null
-    if (!obj.hasOwnProperty(key)) return null
-    // Otherwise, set the object to the object property
-    var o = select(obj[key], key_arr)
-    return o
+    global.elapsed['update_arr'].ms += (performance.now() - timer)
+    return select_obj(obj, key, key_arr)
   }
 
   // If we need to deal with an array, then loop through and return recursive select for each
   if (Array.isArray(obj)) {
-    // Recursively loop through the array and grab the data
-    var o = obj.map( function(o) { return select(o, key_arr.slice()) } )
-    // If we are expecting an array, just return what we did
-    if (ix == '') return o
-    // If we are looking for a specific array element, just return that
-    if (ix) return o[ix]
-    // Otherwise, return the results in the first array element
-    return o.shift()
+    global.elapsed['update_arr'].ms += (performance.now() - timer)
+    return select_arr(obj, key, ix, key_arr)
   }
 }
 
-// Build an array with a data element at the given index
-function build_array(data, ix=0)
+function select_obj(obj, key, key_arr)
 {
-  var arr = Array()
-  var i = (ix) ? ix : 0
-  arr[i] = data
-  return arr
+  global.elapsed['select_obj'].n++ // performance monitoring
+  let timer = performance.now()
+
+  let o = null
+  if (key in obj) {
+    if (key_arr.length > 0) {
+      global.elapsed['select_obj'].ms += (performance.now() - timer)
+      o = select(obj[key], key_arr)
+      timer = performance.now()
+    } else
+      o = obj[key]
+  }
+
+  // Nothing left to process - return the object
+  global.elapsed['select_obj'].ms += (performance.now() - timer)
+  return o
+}
+
+function select_arr(obj, key, ix, key_arr)
+{
+  global.elapsed['select_arr'].n++ // performance monitoring
+  let timer = performance.now()
+
+  // Recursively loop through the array and grab the data
+  obj = obj.map( function(o) {
+    global.elapsed['select_arr'].ms += (performance.now() - timer)
+    return select(o, key_arr.slice())
+    timer = performance.now()
+  })
+  // obj = obj.map( function(o) {
+  //   global.elapsed['select_arr'].ms += (performance.now() - timer)
+  //   return select(o, key_arr.slice())
+  //   timer = performance.now()
+  // })
+  global.elapsed['select_arr'].ms += (performance.now() - timer)
+  // If we are expecting an array, just return what we did
+  if (ix == '') return obj
+  // If we are looking for a specific array element, just return that
+  if (ix) return obj[ix]
+  // Otherwise, return the results in the first array element
+  return obj[0]
 }
 
 // Turns a key string (like key1.key2[].key3 into ['key1','key2','[]','key3']...)
 // 
-function parse(key, delimiter = '.') {
-    // If the to_key is a string, just process the single to_key
-    if (typeof(to_key) == 'string')
-      to_object = update(to_object, to_key, from_data)
-    // If the to_key is an object, process depending on if it is an array of values or just written in object notation
-    if (typeof(to_key) == 'object') {
-      // If the to_key is an array, set the from_key to each of the to_keys
-      if (Array.isArray(to_key))
-        to_key.forEach(function(k) { to_object = update(to_object, k, from_data) })
-      else {
-        // Object notation
-        var k = to_key.key
-        if (k) to_object = update(to_object, k, from_data)
-      }
-    }
-  
-  var key_array = key.split(delimiter)
-  
-  var keys = key_array.reduce(function(keys,current_key) {
-    var [k,ix] = process(current_key)
-    if (k) keys.push(k)
-    if (ix !== null) keys.push('[' + ix + ']')
+function parse(key, delimiter = '.')
+{
+  global.elapsed['parse'].n++ // performance monitoring
+  let timer = performance.now()
+
+  if (typeof(key) == 'string') {
+    var key_array = key.split(delimiter)  
+    var keys = key_array.reduce(function(keys,current_key) {
+      var [k,ix] = process(current_key)
+      if (k) keys.push(k)
+      if (ix !== null) keys.push('[' + ix + ']')
+      return keys
+    }, [])
+    global.elapsed['parse'].ms += (performance.now() - timer)
     return keys
-  }, [])
-  return keys
-}
+  }
+  global.elapsed['parse'].ms += (performance.now() - timer)
+} 
 
 function process(k)
 {
+  global.elapsed['process'].n++ // performance monitoring
+  let timer = performance.now()
+
+  var arr
   try {
     var [_,key,ix] = k.match(/(.*?)\[(.*?)\]/)
-    return [key,ix]
+    arr = [key,ix]
   } catch (e) {
-    return [k,null]
+    arr = [k,null]
   }
+  global.elapsed['parse'].ms += (performance.now() - timer)
+  return arr
 }
 
 // A string of how to navigate through the incoming array is sent.
 // This is translated into an array of instructions for the recursive object
 function getKeyValue(obj, key_str)
 {
+  global.elapsed['getKeyValue'].n++ // performance monitoring
+
   // String
   if (typeof(key_str) == 'string') return select(obj,parse(key_str))
   // Nothing else is valid
@@ -175,6 +239,8 @@ function getKeyValue(obj, key_str)
 
 function setKeyValue(obj,key_str,data)
 {
+  global.elapsed['setKeyValue'].n++ // performance monitoring
+
   // String
   if (Array.isArray(key_str)) {
     key_str.forEach(function(k) {
