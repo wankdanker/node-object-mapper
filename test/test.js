7d8252df74693b6b8c5b3fd760391e5ea408e228
Scott Switzer <switzer@switzer.org>
2019-10-11 17:55:09 -0400

    array mapping wraps destination in an unwanted array #49

--- test/test.js
+++ test/test.js
@@ -1626,12 +1626,12 @@ test('mapping - map and append full array to existing mapped array', function (t
 
   var expect = {
     'thing2' : [
-      [{a: 'a1', b: 'b1'}
+      {a: 'a1', b: 'b1'}
       , {a: 'a2', b: 'b2'}
-      , {a: 'a3', b: 'b3'}],
-      [{a: 'a4', b: 'b4'}
+      , {a: 'a3', b: 'b3'}
+      , {a: 'a4', b: 'b4'}
       , {a: 'a5', b: 'b5'}
-      , {a: 'a6', b: 'b6'}]
+      , {a: 'a6', b: 'b6'}
     ]
   };
 
@@ -2173,5 +2173,32 @@ test('Make sure no objects are created without data #48', function (t) {
   t.end();
 });
 
-
+test('1 array mapping wraps destination in an unwanted array #49', function (t) {
+  const src = {
+    foo: [{ bar: 4 }, { bar: 33 }],
+    bifoo: [{ bar: 77 }, { bar: 97 }]
+  };
+  const mapper = {
+    "foo[].bar": "[]+",
+    "bifoo[].bar": "[]+"
+  }
+  var expect = [4, 33, 77, 97];
+  var result = om(src, mapper);
+  t.deepEqual(result, expect);
+  t.end();
+});
+test('2 array mapping wraps destination in an unwanted array #49', function (t) {
+  const src = {
+    foo: [{ bar: 4 }, { bar: 33 }],
+    bifoo: [{ bar: 77 }, { bar: 97 }]
+  };
+  const mapper2 = {
+    "foo[].bar": "num[]+",
+    "bifoo[].bar": "num[]+"
+  }
+  var expect = {num: [4, 33, 77, 97] };
+  var result = om(src, mapper2);
+  t.deepEqual(result, expect);
+  t.end();
+});
 
